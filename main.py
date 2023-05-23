8e90696895c4bf0b90222674b3c775009a56403b
mena-263 <134085300+mena-263@users.noreply.github.com>
2023-05-23 09:49:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app=FastAPI()
 @app.get("/")
 def index():   
     return"Hola buenas tarde mi nombre es rusel quieres saber de aves "  
-@app.get("/aves")
-def aves ():
-    return "Otro mensaje con aves"
+@app.get("/aves/{num}")
+def aves (num):
+    return num
