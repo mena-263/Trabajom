7f95c5c3c9d45b9b688c3320143fa2019da0c3e9
mena-263 <134085300+mena-263@users.noreply.github.com>
2023-05-23 09:47:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
 def index():   
-    return"Hola buenas tarde mi nombre es rusel quieres saber de aves "
+    return"Hola buenas tarde mi nombre es rusel quieres saber de aves "  
+@app.get("/aves")
+def aves ():
+    return "Otro mensaje con aves"
